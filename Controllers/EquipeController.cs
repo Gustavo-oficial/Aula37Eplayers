1d0eaee90a635511d0c8fea25d5be1280fcecb61
unknown <gmandelinha@gmail.com>
2020-07-15 11:41:47 -0300

    parte 4 e 5 finalizada

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Aula37E_players.Models;
 using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace Aula37E_players.Controllers
 {
@@ -25,11 +26,40 @@ namespace Aula37E_players.Controllers
             Equipe novaEquipe   = new Equipe();
             novaEquipe.IdEquipe = Int32.Parse(form["IdEquipe"]);
             novaEquipe.Nome     = form["Nome"];
-            novaEquipe.Imagem   = form["Imagem"];
+            
+            var file    = form.Files[0];
+            var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                novaEquipe.Imagem   = file.FileName;
+            }
+            else
+            {
+                novaEquipe.Imagem   = "padrao.png";
+            }
 
             equipe.Create(novaEquipe);            
             ViewBag.Equipes = equipe.ReadAll();
 
+            return LocalRedirect("~/Equipe");
+        }
+
+
+          [Route("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            equipe.Delete(id);
+            ViewBag.Equipes = equipe.ReadAll();
             return LocalRedirect("~/Equipe");
         }
     }
--- Controllers/NoticiaController.cs
+++ Controllers/NoticiaController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Aula37E_players.Models;
 using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace Aula37E_players.Controllers
 {
@@ -25,11 +26,39 @@ namespace Aula37E_players.Controllers
             Noticias novaNoticia   = new Noticias();
             novaNoticia.IdNoticia = Int32.Parse(form["IdNoticia"]);
             novaNoticia.Titulo    = form["Titulo"];
-            novaNoticia.Imagem   = form["Imagem"];
+            
+             var file    = form.Files[0];
+            var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Noticias");
+
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                novaNoticia.Imagem   = file.FileName;
+            }
+            else
+            {
+                novaNoticia.Imagem   = "padrao.png";
+            }
 
             noticiaModel.Create(novaNoticia);            
             ViewBag.Noticias = noticiaModel.ReadAll();
 
+            return LocalRedirect("~/Noticia");
+        }
+
+         [Route("{id}")]
+        public IActionResult ExcluirNoticia(int id)
+        {
+            noticiaModel.Delete(id);
+            ViewBag.Noticias = noticiaModel.ReadAll();
             return LocalRedirect("~/Noticia");
         }
     }
